936a55da9e165020d211601aced9295c3f6390dc
Björn Daase <bjoern.daase@student.hpi.de>
2018-10-27 12:34:14 +0200

    Fix #4

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,20 @@
 class AuthorsController < ApplicationController
+	def show
+   	  @author = Author.find(params[:id])
+  	end
+
 	def new
 	end
+
+	def create
+	  @author = Author.new(author_params)
+	 
+	  @author.save
+	  redirect_to @author
+	end
+
+	private
+	  def author_params
+	    params.require(:author).permit(:first_name, :last_name, :homepage)
+	  end
 end
