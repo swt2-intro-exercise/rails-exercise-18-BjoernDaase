02f117087e6f932c39829e2074c59bc08143a00a
Björn Daase <bjoern.daase@student.hpi.de>
2018-10-27 15:17:13 +0200

    Add test for #8

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,18 +2,21 @@ require 'rails_helper'
 
  describe "Author model", type: :model do
 
-   before :each do
-    @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
-  end
-
    it "should create new author with first name, last name and homepage" do
+    @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
     expect(@author.first_name).to eq("Alan")
     expect(@author.last_name).to eq("Turing")
     expect(@author.homepage).to eq("http://wikipedia.org/Alan_Turing")
   end
 
    it "should have a name which combines first name and last name" do
+    @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
     expect(@author.name).to eq("Alan Turing")
+  end
+
+  it "should require that the last name is not empty" do
+    @author = Author.new(first_name: "Alan", last_name: nil, homepage: "http://wikipedia.org/Alan_Turing")
+    expect(@author).to_not be_valid
   end
 
 end 
