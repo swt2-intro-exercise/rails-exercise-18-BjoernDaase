14303917fd7222b85c38ab9ee3133b683a8c99e3
Björn Daase <bjoern.daase@student.hpi.de>
2018-10-28 10:46:34 +0100

    Add failing test for #11

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,8 +1,19 @@
-describe 'Edit author page', type: :feature do
+describe "Edit author page", type: :feature do
 
-   it 'should render without error' do
+   it "should render without error" do
     @author = FactoryBot.create :author
     visit edit_author_path(@author)
   end
 
+  it "should save changes" do
+    @author = FactoryBot.create :author
+    visit new_author_path(@author)
+    page.fill_in 'author[first_name]', with: 'Joe'
+    page.fill_in 'author[last_name]', with: 'Editing'
+    page.fill_in 'author[homepage]', with: 'http://joe.edit'
+    find('input[type="submit"]').click
+    expect(@author.first_name).to eq('Joe')
+    expect(@author.last_name).to eq('Editing')
+    expect(@author.homepage).to eq('http://joe.edit')
+  end
 end
